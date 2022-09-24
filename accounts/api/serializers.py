f122f5914ac04b9aa6059e0a169b58521901d077
Dongzhou Yu <dongzhou_yu@yahoo.com>
2022-09-24 17:56:25 -0400

    feat: 🎸 add login, get login sIPA pu ngis ,tuogol ,sutatslogin

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,7 +1,46 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['username', 'email']
+
+class LoginSerializer(serializers.Serializer):
+    # check if username and password exists
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    # will be called when is_valid is called
+    def validate(self, data):
+        if User.objects.filter(username=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+               'message': 'This email address has been used'
+            })
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This username has been used'
+            })
+        return data
+
+    def create(selfself, validated_data):
+        # save as lower case
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email
+        )
+        return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from accounts.api.serializers import (
+    UserSerializer,
+    LoginSerializer,
+    SignupSerializer,
+)
+from django.contrib.auth import (
+    logout as django_logout,
+    login as django_login,
+    authenticate as django_authenticate,
+)
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -11,3 +21,73 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+    serializer_class = SignupSerializer
+    # setting detail=False to make sure the action is for the directory instead of a single object
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {
+            'has_logged_in': request.user.is_authenticated
+        }
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        # get username and password from request
+        serializer = LoginSerializer(data=request.data)
+        # validate user input
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=400)
+
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        # validate if account exists
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                'success': False,
+                'message': 'User does not exists',
+                'errors': serializer.errors,
+            }, status=400)
+        user = django_authenticate(request, username=username, password=password)
+        # validate user is not anonymous
+        if not user or user.is_anonymous:
+            return Response({
+                'success': False,
+                'message': 'Username and password does not match'
+            }, status=400)
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(instance=user).data,
+        })
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        # if instance argument is passed, an update is performed
+        # if not, creation is performed
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input.',
+                'errors': serializer.errors,
+            }, status=400)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data
+        }, status=201)
